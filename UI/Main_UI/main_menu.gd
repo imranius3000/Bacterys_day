cf5406368c4756cad3d68d24ebd1883490139942
wHOIAMZZ <luxuremainel@gmail.com>
2026-01-21 21:45:05 +0300

    e

--- UI/Main_UI/main_menu.gd
+++ UI/Main_UI/main_menu.gd
@@ -1,5 +1,5 @@
 extends CanvasLayer
-
+var settings_menu = preload("res://UI/Main_UI/settings_menu.tscn")
 
 
 #Кнопка Играть
@@ -8,7 +8,8 @@ func _on_play_button_pressed():
 
 #Кнопка Настроек
 func _on_settings_button_pressed():
-	get_tree().change_scene_to_file("res://UI/main_ui/settings_menu.tscn")
+	var settings_menu_instance = settings_menu.instantiate()
+	add_child(settings_menu_instance)
 
 #Кнопка выхода
 func _on_quit_button_pressed():
--- UI/Main_UI/settings_menu.gd
+++ UI/Main_UI/settings_menu.gd
@@ -3,13 +3,15 @@ extends CanvasLayer
 
 
 
-
+#Закрыть настройки
 func _on_return_button_pressed():
 	queue_free()
 
+#Вызваем сразу функцию обновления настроек
 func _ready():
 	update_options()
-	
+
+#Функция для обновлений настроек, при изменение звука или режима экрана
 func update_options():
 	if DisplayServer.window_get_mode() == DisplayServer.WINDOW_MODE_FULLSCREEN:
 		resolution_button.text = "FULL SCREEN"
@@ -17,7 +19,7 @@ func update_options():
 		resolution_button.text = "WINDOWED"
 
 
-
+#Изменить режим окна
 func _on_resolution_button_pressed():
 	var mode = DisplayServer.window_get_mode()
 	if mode != DisplayServer.WINDOW_MODE_FULLSCREEN:
